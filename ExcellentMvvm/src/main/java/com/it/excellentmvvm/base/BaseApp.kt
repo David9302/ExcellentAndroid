e90524c3d2d70236e287221305603462652dc2bc
dengluoy <dengluoy@github.com>
2025-03-16 00:02:42 +0800

    first commit

---
+++ ExcellentMvvm/src/main/java/com/it/excellentmvvm/base/BaseApp.kt
@@ -0,0 +1,13 @@
+package com.it.excellentmvvm.base
+
+import android.app.Application
+
+/**
+
+ * @Author David
+ * @Date 2025/3/12 00:36
+ * @Email davidhello6@gmail.com
+ * @Desc todo
+ */
+open class BaseApp : Application(){
+}
---
+++ app/src/main/java/com/it/excellent/App.kt
@@ -0,0 +1,15 @@
+package com.it.excellent
+
+import android.app.Application
+import android.content.Context
+
+/**
+
+ * @Author David
+ * @Date 2025/3/12 00:25
+ * @Email davidhello6@gmail.com
+ * @Desc todo
+ */
+class App : Application() {
+
+}
